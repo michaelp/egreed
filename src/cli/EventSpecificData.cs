e3b9d6fc3c4da937966c0ccca192ad3dc1639e01
Michael Pavlovsky <michael.pavlovsky@gmail.com>
2020-11-22 20:54:14 -0500

    wip

---
+++ src/cli/EventSpecificData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cli
+{
+    static partial class Program
+    {
+        private class EventSpecificData
+        {
+            public string Message { get; internal set; }
+            public string FormId { get; internal set; }
+            public int SomeInt { get; internal set; }
+            public DateTime SomeDate { get; internal set; }
+        }
+    }
+}
--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace cli
 {
-    class Program
+    internal static partial class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            
+            var topicHostname = new Uri(topicEndpoint).Host;
+            var topicCredentials = new TopicCredentials(topicKey);
+            var ct = new CancellationTokenSource();
+            using (var client = new EventGridClient(topicCredentials))
+            {
+
+                var msg = string.Empty;
+                while ("q" != (msg = Console.ReadLine()))
+                {
+                    await client.PublishEventsAsync(topicHostname, GetEvents(msg), ct.Token);
+                    Console.WriteLine($"sent:[{msg}] ");
+                }
+                ct.Cancel();
+            }
+
+
         }
+
+        private static IList<EventGridEvent> GetEvents(string msg = "") => new[] {
+                new EventGridEvent()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Data = new EventSpecificData()
+                    {
+                        Message = msg,
+                        FormId = "Grading/WorkersCompensation",
+                        SomeInt = 10,
+                        SomeDate = DateTime.Now
+                    },
+                    EventTime = DateTime.Now,
+                    EventType = "Task.Form.Completed",
+                    Subject = msg,
+                    DataVersion = "1.0"
+                }
+            };
     }
 }
